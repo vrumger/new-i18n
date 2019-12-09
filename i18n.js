8dfd0896293a918f2cedfb0829c277377bfd1c0e
YouTwitFace <contact@lungers.com>
2019-12-08 19:22:41 -0500

    Use a class instead of a function (backwards compatible)

---
+++ i18n.js
@@ -0,0 +1,57 @@
+class I18n extends Function {
+    constructor(folder, languages, fallback = null) {
+        if (!languages || (Array.isArray(languages) && languages.length === 0)) {
+            throw new Error(`You need to add at least one language.`);
+        }
+
+        super();
+        this._fallback = fallback;
+        this._languages = languages.reduce((languages, language) => {
+            languages[language] = require(`${folder}/${language}.json`);
+            return languages;
+        }, {});
+
+        return new Proxy(this, {
+            apply(target, _, [language, keyword, variables = {}]) {
+                return target.translate(language, keyword, variables);
+            },
+        });
+    }
+
+    get languages() {
+        return Object.keys(this._languages);
+    }
+
+    get fallback() {
+        return this._fallback;
+    }
+
+    set fallback(fallback) {
+        this._fallback = fallback;
+    }
+
+    translate(language, keyword, variables = {}) {
+        if (!this._languages[language]) {
+            return keyword;
+        }
+
+        const value = keyword
+            .split(`.`)
+            .reduce((res, key) => res && res[key], this._languages[language]);
+
+        if (!value) {
+            if (this._fallback && language !== this._fallback) {
+                return this.translate(this._fallback, keyword, variables);
+            }
+
+            return keyword;
+        }
+
+        return value.replace(
+            /\{{2}(.+?)\}{2}/g,
+            (_, variable) => variables[variable] || variable,
+        );
+    }
+}
+
+module.exports = I18n;
--- index.js
+++ index.js
@@ -1,40 +1,6 @@
 `use strict`;
 
-module.exports = (folder, langs, fallback = null) => {
-    if (!langs || (Array.isArray(langs) && langs.length === 0)) {
-        throw new Error(`You need to add at least one language.`);
-    }
+const I18n = require(`./i18n`);
 
-    const languages = {};
-
-    langs.forEach(lang => (languages[lang] = require(`${folder}/${lang}.json`)));
-
-    const i18n = function (lang, keyword, variables = {}) {
-        if (!languages[lang]) {
-            return keyword;
-        }
-
-        const value = keyword
-            .split(`.`)
-            .reduce(
-                (res, key) => res && res[key],
-                languages[lang]
-            );
-
-        if (!value) {
-            if (fallback && lang !== fallback) {
-                return i18n(fallback, keyword, variables);
-            } else {
-                return keyword;
-            }
-        }
-
-        return  value.replace(
-            /\{{2}(.+?)\}{2}/g,
-            (_, variable) => variables[variable] || variable
-        );
-    };
-
-    i18n.languages = Object.keys(languages);
-    return i18n;
-};
+module.exports = (folder, languages, fallback = null) =>
+    new I18n(folder, languages, fallback);
